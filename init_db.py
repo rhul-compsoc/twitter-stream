09bff4d03e17c537f715a4a8230a570a4f3e04fe
chazzox <42206116+chazzox@users.noreply.github.com>
2021-11-12 23:17:46 +0000

    Refactors init_db.py

--- init_db.py
+++ init_db.py
@@ -2,8 +2,6 @@ import sqlite3
 from constants import DATABASE_FILE, ENDPOINT_URL, SEARCH_QUERY
 from twitter import search
 
-connection = sqlite3.connect(DATABASE_FILE)
-
 
 def dummy_value():
     res = search(ENDPOINT_URL, SEARCH_QUERY)
@@ -21,48 +19,36 @@ def dummy_value():
     }
 
 
-def main():
-    print('Opened database successfully in file "{}"'.format(DATABASE_FILE))
+connection = sqlite3.connect(DATABASE_FILE)
+print('Opened database successfully in file "{}"'.format(DATABASE_FILE))
 
-    cursor = connection.cursor()
-
-    # checking to see if any table exists
-    cursor.execute(
-        "SELECT name FROM sqlite_master WHERE type='table'")
-
-    if len(cursor.fetchall()):
-        overwrite = input("Table already exists, overwrite? (y/n): ")
-        if overwrite != 'y':
-            return
-        # clearing table
-        connection.execute(' DROP TABLE tweets;')
-
-    connection.execute(
-            """CREATE TABLE IF NOT EXISTS tweets (
-	TWEET_ID                        VARCHAR(10),
-	TWEET_TEXT                      VARCHAR(10),
-	TWEET_AUTHOR_USERNAME           VARCHAR(10),
-	TWEET_AUTHOR_NAME				VARCHAR(10),
-	TWEET_TIME                      VARCHAR(10),
-	AUTHORIZED						INTEGER DEFAULT 0
+connection.execute(
+    """CREATE TABLE IF NOT EXISTS tweets (
+TWEET_ID                        VARCHAR(10),
+TWEET_TEXT                      VARCHAR(10),
+TWEET_AUTHOR_USERNAME           VARCHAR(10),
+TWEET_AUTHOR_NAME				VARCHAR(10),
+TWEET_TIME                      VARCHAR(10),
+AUTHORIZED						INTEGER DEFAULT 0
 )""")
 
+cursor = connection.cursor()
+cursor.execute("SELECT * FROM tweets LIMIT 1")
+rows = cursor.fetchall()
+
+if len(rows) == 0:
     create_dummy_values = input(
         "Table empty, create dummy values? (y/n): ")
-    if create_dummy_values != 'y':
-        return
 
-    print('Creating dummy values in database...')
-    dummy_tweet = dummy_value()
-    dummy_values = ', '.join(
-        map(lambda a: f"'{str(a)}'", dummy_tweet.values()))
-    cursor.execute(
-        f"INSERT INTO tweets ({', '.join(dummy_tweet.keys())}) VALUES ({dummy_values})")
-    connection.commit()
+    if(create_dummy_values == 'y' or create_dummy_values == 'yes'):
+        print('Creating dummy values in database...')
+        dummy_tweet = dummy_value()
+        dummy_values = ', '.join(
+            map(lambda a: f"'{str(a)}'", dummy_tweet.values()))
+        cursor.execute(
+            f"INSERT INTO tweets ({', '.join(dummy_tweet.keys())}) VALUES ({dummy_values})")
+        connection.commit()
 
-
-if __name__ == "__main__":
-    main()
 
 print('Closing connection to database')
 connection.close()

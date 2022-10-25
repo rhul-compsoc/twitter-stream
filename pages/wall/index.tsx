08b2dc9eae9866b1b95b4f5ffbea4cfe370fe957
James <jamesarnott2@gmail.com>
2022-10-25 16:34:22 +0100

    fix: grid now is not broken, some danny level css jankyness was used

--- pages/wall/index.tsx
+++ pages/wall/index.tsx
@@ -5,10 +5,10 @@ import { NextPageWithLayout } from '../_app';
 const wall: NextPageWithLayout = () => {
     return (
         <>
-            <div className="flex-1 p-20">
-                <div className="w-full h-full bg-slate-700 shadow-2xl rounded-2xl wallContainer overflow-clip">
+            <div className="flex-1 pt-20 pb-20 flex justify-center align-middle">
+                <div className="w-11/12 h-full bg-slate-700 shadow-2xl rounded-2xl wallContainer overflow-hidden m-auto">
                     <div className="flex mt-6 flex-1">
-                        <div className="text-4xl m-auto text-shadow-xl shadow-red text-black font-bold text-center ">
+                        <div className="text-4xl m-auto text-shadow-xl shadow-red text-black font-bold text-center">
                             The 80s90s00s Twitter Feed
                         </div>
                     </div>
@@ -17,10 +17,10 @@ const wall: NextPageWithLayout = () => {
                             I am a tweet!
                         </div>
                     </div>
-                    {/* Need to hide the overflow whilst having the position as absolute, still a work in progress */}
-                    <div className="overflow-hidden bg-black absolute bottom-[10vh] w-full">
-                        <div className="h-32 align-middle relative top-[-10vh]">
-                            <div className="movingGridConfig animatedGrid m-auto"></div>
+                    {/* Need to hide the overflow whilst having the position as absolute, still a work in progress overflow-hidden*/}
+                    <div className="bg-black absolute bottom-[10vh] w-11/12 overflow-hidden z-10 rounded-b-2xl ">
+                        <div className="h-32 align-middle relative">
+                            <div className="movingGridConfig animatedGrid m-auto top-[-17em] absolute"></div>
                         </div>
                         <div className="text-white text-2xl">
                             hi

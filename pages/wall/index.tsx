0891b694929a0fb3ed04f57c27ffb1eee3be07f7
James <jamesarnott2@gmail.com>
2022-10-27 21:30:31 +0100

    refactor: changed text, added comments

--- pages/wall/index.tsx
+++ pages/wall/index.tsx
@@ -3,7 +3,7 @@ import MainLayout from '../../components/layouts/mainLayout';
 import { NextPageWithLayout } from '../_app';
 import VaporWave from './themes/VaporWave';
 
-const tweet = 'It’s hard to imagine the internet without the constant buzz of Twitter, the popular social media platform on which hundreds of millions of short soundbites from politicians, celebrities, and other online users are shared out each day.As a platform known for brevity and concisionn';
+const tweet = 'Joshua Yewman, have my babies! <3';
 const tweet2 = 'It’s hard to imagine the internet without the constant buzz of Twitter';
 const tweet3 = 'hkbgdfilhugfdshiulgrefklhjfgdskjlhfgsdjklhfgdslkhjfgsdbkjmn,bdf,mnbvcxjn,fgdjkl;fgsdhjl;ndfgshjlk;nfgsdhjlk;dfgshjlk;dfgsl;kmdfghklj;ngdfsljn;dfgsjlk;ndfgsl;jnkfgdsl;jnfgsd;jnlksfgdjnl;ksdfg;jnldfgsjkln;dfgsjl;kndfgskl;jnsdfgkjlndfgskjlnsdfgkljnsdfgjklndsfg';
 
@@ -11,7 +11,10 @@ const wall: NextPageWithLayout = () => (
   <>
     <div className="flex-1 p-20 flex justify-center align-middle">
       <div className="w-full h-full bg-slate-700 shadow-2xl rounded-2xl overflow-hidden m-auto relative">
-        <VaporWave tweet={tweet3} topBanner="The 80s90s00s Twitter Feed!" />
+        {/**
+         * This is the vaporwave theme for the wall. We should have it rotate through different themes imo.
+         */}
+        <VaporWave tweet={tweet} topBanner="The 80s90s00s Twitter Feed!" />
       </div>
     </div>
 

f1ff8f68263d5420a6862113799b68c164739e4d
James <jamesarnott2@gmail.com>
2022-12-02 18:51:22 +0000

    chore; added temp tweet

--- pages/wall.tsx
+++ pages/wall.tsx
@@ -19,7 +19,18 @@ const fetchTimeline = async () => {
 };
 
 const Wall: NextPageWithLayout = () => {
-  const [tweetsArr, setTweets] = useState<(tweets)[]>([]);
+  const [tweetsArr, setTweets] = useState<(tweets)[]>([{
+    tweet_id: '1598748206057209856',
+    tweet_text: '#80s90s00s is sick',
+    tweet_created_at: '2022-12-02T18:37:30.000Z',
+    tweet_author_id: '1489248049855557633',
+    tweet_author_name: 'James Arnott',
+    tweet_author_username: 'JamesArnott21',
+    tweet_author_created_at: '2022-12-02T18:37:30.000Z',
+    tweet_author_profile_image_url: 'https://pbs.twimg.com/profile_images/1489248111725740039/1kmLtVly_normal.png',
+    process_result: 'v',
+    processed_at: null,
+  }]);
   const [scene, setScene] = useState(0);
   const [currentTweet, setCurrentTweet] = useState<number>(0);
   const [gotInitialTweets, setGotInitialTweets] = useState(false);
@@ -59,9 +70,9 @@ const Wall: NextPageWithLayout = () => {
       }, 5 * 60 * 1000);
 
       setInterval(() => {
-        const nextTweet = (currentTweet + 1) % (tweetsArr.length === 0 ? 1 : tweetsArr.length);
+        const nextTweet = (currentTweet + 1) % tweetsArr.length;
         setCurrentTweet(nextTweet);
-      }, 15 * 1000);
+      }, 1 * 1000);
       setGotInitialTweets(true);
     }
   }, [tweetsArr]);
@@ -71,18 +82,20 @@ const Wall: NextPageWithLayout = () => {
       <div className="flex flex-1 justify-center p-20 align-middle">
         <div className="relative m-auto h-full w-full overflow-hidden rounded-2xl bg-slate-700 shadow-2xl">
           {
-            (scene === 0 && tweetsArr.length > 0) && (
+            scene === 0 && (
               <VaporWave
                 tweet={tweetsArr[currentTweet].tweet_text}
+                key={currentTweet}
                 // authorName={tweetsArr[currentTweet].author_name}
                 topBanner="The 80s90s00s Twitter Feed!"
               />
             )
           }
           {
-            (scene === 1 && tweetsArr.length > 0) && (
+            scene === 1 && (
               <Ninteens
                 tweet={tweetsArr[currentTweet].tweet_text}
+                key={currentTweet}
                 // authorName={tweetsArr[currentTweet].author_name}
                 topBanner="The 80s90s00s Twitter Feed!"
               />

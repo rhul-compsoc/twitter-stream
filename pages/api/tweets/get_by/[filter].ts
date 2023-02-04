dee740e7f442bafbe3520f994cec371c68973b5b
chazzox <caylott@outlook.com>
2023-02-04 23:23:37 +0000

    adding more routes

---
+++ pages/api/tweets/get_by/[filter].ts
@@ -0,0 +1,5 @@
+import { NextApiHandler } from 'next';
+
+const GetTweetByFilter: NextApiHandler = async (req, res) => {};
+
+export default GetTweetByFilter;
---
+++ pages/api/tweets/process.ts
@@ -0,0 +1,5 @@
+import { NextApiHandler } from 'next';
+
+const Process: NextApiHandler = async (req, res) => {};
+
+export default Process;
